8fcd28e8f2ebff2aa9bd6d8889a88a81a4144a8f
Mariia Paraketsova <paraketsova@gmail.com>
2021-11-03 21:29:47 +0100

    Update fetch with loading message at AllTodoListsPage

--- client/src/pages/AllTodoListsPage.js
+++ client/src/pages/AllTodoListsPage.js
@@ -4,12 +4,16 @@ import {Link} from 'react-router-dom';
 
 export default function AllTodoListsPage() {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  const fetchData = () => {
+
+  const fetchData = async () => {
+    setLoading(true);
     const url = 'http://localhost:3000/api/todo/lists';
-    fetch(url)
-      .then(res => res.json())
-      .then(data => setData(data))
+    const res = await fetch(url);
+    const data = await res.json();
+    setData(data);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -18,26 +22,36 @@ export default function AllTodoListsPage() {
 
   return (
     <>
-      <h1>Your TODOlist</h1>
-      <table>
-        <thead>
+      <header>
+        <h1>Your TODOlist</h1>
+      </header>
+      {loading ? (
+        <p className="load">Loading...</p>
+      ) : (
+        <table>
+          <thead>
           <tr>
             <th className="col1">Title</th>
             <th className="col2">Contents</th>
             <th className="col3">Edit task</th>
           </tr>
-        </thead>
-        <tbody>
-        {console.log(data)}
-          { data && data.map((todoList, index) => {
-            {console.log(todoList)}
-            {Object.entries(todoList).forEach((prop)=> console.log(prop))}
+          </thead>
+          <tbody>
+          {/*{console.log(data)}*/}
+          { data && data.map((todoList) => {
+            // {console.log(todoList)}
+            // {Object.entries(todoList).forEach((prop)=> console.log(prop))}
             // {console.log(JSON.stringify(todoList, null, 4))}
             // {console.log(todoList.title)}
 
             return (
               <tr key={todoList._id}>
-                <td className="col1">{todoList.title}</td>
+                <td className="col1">
+                  <label className="checkbox" htmlFor={todoList.title}>
+                    <input type="checkbox" id={todoList.title} name={todoList.title}/>
+                    {todoList.title}
+                  </label>
+                </td>
                 <td className="col2">{todoList.contents}</td>
                 <td className="col3">
                   <button>
@@ -50,8 +64,9 @@ export default function AllTodoListsPage() {
               // </div>
             )
           })}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      )}
     </>
   )
 }
