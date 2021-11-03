e5630745c277d015e58d7d31b4b3cafa9cbe9694
Mariia Paraketsova <paraketsova@gmail.com>
2021-11-03 12:52:43 +0100

    Update todo at AllTodoListsPage to table.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import AllTodoListsPage from './pages/AllTodoListsPage';
 //import ListDetailsPage from './pages/ListDetailsPage';
+import './App.css';
 
 function App() {
   return (
--- client/src/pages/AllTodoListsPage.js
+++ client/src/pages/AllTodoListsPage.js
@@ -16,20 +16,34 @@ export default function AllTodoListsPage() {
   }, [])
 
   return (
-    <div>
-      { data && data.map((todoList, index) => {
-        {console.log(todoList)}
-        {Object.entries(todoList).forEach((prop)=> console.log(prop))}
-        {console.log(JSON.stringify(todoList, null, 4))}
-        {console.log(todoList.title)}
+    <>
+      <h1>Your TODOlist</h1>
+      <table>
+        <thead>
+          <tr>
+            <th className="col1">Title</th>
+            <th className="col2">Contents</th>
+          </tr>
+        </thead>
+        <tbody>
+          { data && data.map((todoList, index) => {
+            //{console.log(todoList)}
+            // {Object.entries(todoList).forEach((prop)=> console.log(prop))}
+            // {console.log(JSON.stringify(todoList, null, 4))}
+            // {console.log(todoList.title)}
 
-        return (
-          <div>
-            <h3> TodoList:</h3>
-            <List key={index} {...todoList}/>
-          </div>
-        )
-      })}
-    </div>
+            return (
+              <tr key={todoList._id}>
+                <td className="col1">{todoList.title}</td>
+                <td className="col2">{todoList.contents}</td>
+              </tr>
+              // <div key={todoList._id}>
+              //   <List key={index} {...todoList}/>
+              // </div>
+            )
+          })}
+        </tbody>
+      </table>
+    </>
   )
 }
