ffd8c99a1b3754ea7a4cdca2846bfa67fa3d3f03
Mariia Paraketsova <paraketsova@gmail.com>
2021-11-03 09:23:20 +0100

    Add client - dependencies, test page.

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import AllTodoListsPage from './pages/AllTodoListsPage';
+//import ListDetailsPage from './pages/ListDetailsPage';
+
+function App() {
+  return (
+
+    <div className="container">
+      {/*/<Switch>*/}
+        {/*<Route path="/:id" component={ListDetailsPage}/>*/}
+        <Route path="/"><AllTodoListsPage/></Route>
+      {/*</Switch>*/}
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ client/src/pages/AllTodoListsPage.js
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from 'react';
+//
+export default function AllTodoListsPage() {
+//   const [data, setData] = useState(null);
+//
+//   const fetchData = () => {
+//     const url = '/api/todo/lists';
+//     fetch(url)
+//       .then(res => res.json())
+//       .then(data => setData(data))
+//   };
+//
+//   useEffect(() => {
+//     fetchData()
+//   }, [])
+
+  return (
+    <div>
+      <p>lalala</p>
+      {/*{ data && data.map(([key, value]) => (*/}
+      {/*  return (*/}
+      {/*    <div key={key}>*/}
+      {/*      <h3>{key}</h3>*/}
+      {/*      <p>{value}</p>*/}
+      {/*      <p>Latest updated: {lastModifiedAt}</p>*/}
+      {/*      /!*<div key={index} {...todoList}/>*!/*/}
+      {/*    </div>*/}
+      {/*  )*/}
+      {/*))}*/}
+    </div>
+  )
+}
