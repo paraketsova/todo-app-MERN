81867b15849e3d8b66658c8df1405fab7cfd3cf9
Mariia Paraketsova <paraketsova@gmail.com>
2021-11-12 17:13:06 +0100

    Add error's message to routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,20 +8,43 @@ router.get('/favicon.ico', async function (req, res) {
 });
 
 /*  READ all lists  */
-router.get('/', async function (req, res, next) {
+router.get('/', async function (req, res) {
   const todoLists = await todoItem.find();
-  res.json(todoLists);
+
+  if (!todoLists) {
+    res.statusCode = 404;
+    res.statusMessage = 'Not Found'
+    res.end('Something wrong');
+  } else {
+    res.statusCode = 200;
+    res.json(todoLists);
+  }
 });
 
 /*  READ one list (with all its tasks)  */
-router.get('/:id', async function (req, res, next) {
+router.get('/:id', async function (req, res) {
   const { id } = req.params;
-  const todoList = await todoItem.findOne({ _id: id });
-  res.json(todoList);
+  if (!id) {
+    res.statusCode = 500;
+    res.statusMessage = 'Invalid id';
+    res.end('Invalid id');
+  } else {
+    //  Find TODOlist by Id
+    const todoList = await todoItem.findOne({_id: id});
+
+    if (!todoList) {
+      res.statusCode = 404;
+      res.statusMessage = 'Not Found'
+      res.end('Not found');
+    } else {
+      res.statusCode = 200;
+      res.json(todoList);
+    }
+  }
 });
 
 /*  ADD new list  */
-router.post('/lists/add', async function (req, res, next) {
+router.post('/lists/add', async function (req, res) {
   console.log(req.body.newTitle);
   const newTitle = req.body.newTitle;
   const newList = await todoItem.create({
@@ -33,7 +56,7 @@ router.post('/lists/add', async function (req, res, next) {
 });
 
 /*  ADD new task  */
-router.post('/tasks/add/:id', async function (req, res, next) {
+router.post('/tasks/add/:id', async function (req, res) {
   console.log(req.body.newText, req.params.id);
   const id = req.params.id;
   const newText = req.body.newText;
@@ -51,73 +74,156 @@ router.post('/tasks/add/:id', async function (req, res, next) {
 });
 
 /*  UPDATE list title  */
-router.put('/lists/update/:id', async function (req, res, next) {
+router.put('/lists/update/:id', async function (req, res) {
   console.log(req.body.title, req.params.id);
   const id = req.params.id;
   const title = req.body.title;
-  const editedTodoList = await todoItem.findOneAndUpdate(
-    { _id: id },
-    { title },
-    { new: true }
-  );
-  res.json(editedTodoList); //TODO - add to client or delete
+
+  if (!id) {
+    res.statusCode = 500;
+    res.statusMessage = 'Invalid id';
+    res.end('Invalid id');
+  } else {
+    //  Find TODOlist and update its title
+    const editedTodoList = await todoItem.findOneAndUpdate(
+      { _id: id },
+      { title },
+      { new: true }
+    );
+
+    if (!editedTodoList) {
+      res.statusCode = 404;
+      res.statusMessage = 'List Not Found';
+      res.end('List Not found');
+    } else {
+      res.statusCode = 200;
+      res.json(editedTodoList);
+    }
+  }
 });
 
 /*  UPDATE task text  */
-router.put('/tasks/update/:id', async function (req, res, next) {
+router.put('/tasks/update/:id', async function (req, res) {
   const id = req.params.id;
   const newTaskText = req.body.newTaskText;
   const i = req.body.i;
 
-  const { tasks } = await todoItem.findOne({ _id: id });
-  tasks[i].text = newTaskText;
+  if (!id) {
+    res.statusCode = 500;
+    res.statusMessage = 'Invalid id';
+    res.end('Invalid id');
+  } else {
+    if (!i) {
+      res.statusCode = 500;
+      res.statusMessage = 'Invalid task';
+      res.end('Invalid task');
+    } else {
+      //   Find TODOList in DB
+      const { tasks } = await todoItem.findOne({_id: id});
+      tasks[i].text = newTaskText;
+      // Update one task's text in TODOlist
+      const updatedTodoList = await todoItem.findOneAndUpdate(
+        {_id: id},
+        {tasks},
+        {new: true}
+      );
 
-  const updatedTodotList = await todoItem.findOneAndUpdate(
-    { _id: id },
-    { tasks },
-    { new: true }
-  );
-  res.json(updatedTodotList);
+      if (!updatedTodoList) {
+        res.statusCode = 404;
+        res.statusMessage = 'List Not Found';
+        res.end('List Not found');
+      } else {
+        res.statusCode = 200;
+        res.json(updatedTodoList);
+      }
+    }
+  }
 });
 
 /*  UPDATE task status  */
-router.put('/tasks/complete/:id', async function (req, res, next) {
+router.put('/tasks/complete/:id', async function (req, res) {
   const id = req.params.id;
   const i = req.body.i;
   const newStatus = req.body.newStatus;
 
-  const { tasks } = await todoItem.findOne({ _id: id });
-  tasks[i].completed = newStatus;
+  if (!id) {
+    res.statusCode = 500;
+    res.statusMessage = 'Invalid id';
+    res.end('Invalid id');
+  } else {
+    // Find TODOList in DB
+    const { tasks } = await todoItem.findOne({ _id: id });
+    tasks[i].completed = newStatus;
 
-  const updatedTodotList = await todoItem.findOneAndUpdate(
-    { _id: id },
-    { tasks },
-    { new: true }
-  );
-  res.json(updatedTodotList);
+    // Update one task in TODOlist and update the status
+    const updatedTodoList = await todoItem.findOneAndUpdate(
+      { _id: id },
+      { tasks },
+      { new: true }
+    );
+
+    if (!updatedTodoList) {
+      res.statusCode = 404;
+      res.statusMessage = 'List Not Found';
+      res.end('List Not found');
+    } else {
+      res.statusCode = 200;
+      res.json(updatedTodoList);
+    }
+  }
 });
 
 /*  DELETE list  */
-router.delete('/lists/delete/:id', async function (req, res, next) {
+router.delete('/lists/delete/:id', async function (req, res) {
   const id = req.params.id;
-  const deletedList = await todoItem.findOneAndDelete(
-    { _id: id }
-  );
-  res.json(deletedList);
+  if (!id) {
+    res.statusCode = 500;
+    res.statusMessage = 'Invalid id';
+    res.end('Invalid id');
+  } else {
+    // Delete TODOlist from DB
+    const deletedList = await todoItem.findOneAndDelete(
+      {_id: id}
+    );
+
+    if (!deletedList) {
+      res.statusCode = 404;
+      res.statusMessage = 'Not Found'
+      res.end('Not found');
+    } else {
+      res.statusCode = 200;
+      res.json(deletedList);
+    }
+  }
 });
 
 /*  DELETE task  */
-router.delete('/tasks/delete/:id/', async function (req, res, next) {
+router.delete('/tasks/delete/:id/', async function (req, res) {
   const id = req.params.id;
   const index = req.body.i;
-  const { tasks } = await todoItem.findOne({ _id: id });
-  const newTasks = tasks.splice(index, 1);
-  const updateTodoList = await todoItem.findOneAndUpdate(
-    { _id: id },
-    { tasks },
-    { new: true }
-  );
-  res.json(updateTodoList);
+  if (!id) {
+    res.statusCode = 500;
+    res.statusMessage = 'Invalid id';
+    res.end('Invalid id');
+  } else {
+    // Delete task from tasks' array
+    const {tasks} = await todoItem.findOne({_id: id});
+    const newTasks = tasks.splice(index, 1);
+    const updatedTodoList = await todoItem.findOneAndUpdate(
+      {_id: id},
+      {tasks},
+      {new: true}
+    );
+
+    if (!updatedTodoList) {
+      res.statusCode = 404;
+      res.statusMessage = 'List Not Found';
+      res.end('List Not found');
+    } else {
+      res.statusCode = 200;
+      res.json(updatedTodoList);
+    }
+  }
 });
 
 module.exports = router;
